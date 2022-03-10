f1f3a9daa4f9203d0b43e1b32edbdaded38d783e
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-03-10 21:10:33 +0700

    replace liveData on Flow

--- feature_year/src/main/java/com/eg/feature_year/YearViewModel.kt
+++ feature_year/src/main/java/com/eg/feature_year/YearViewModel.kt
@@ -1,26 +1,38 @@
 package com.eg.feature_year
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.eg.cars.main.base.BaseViewModel
+import com.eg.core.utils.setPostValue
 import com.eg.domain.entity.Cars
+import com.eg.domain.entity.Resource
 import com.eg.domain.entity.Year
 import com.eg.domain.use_case.GetYearsUseCase
-import com.eg.core.utils.setPostValue
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class YearViewModel @Inject constructor(private val useCase: GetYearsUseCase) : ViewModel() {
+class YearViewModel @Inject constructor(private val useCase: GetYearsUseCase) : BaseViewModel() {
 
-
-    val years: LiveData<List<Year>> by lazy {
-        MutableLiveData()
-    }
+    val years: Flow<List<Year>> by lazy { MutableSharedFlow(replay = 1) }
 
     fun fetchYear(cars: Cars?) = viewModelScope.launch {
-        years.setPostValue(useCase.invoke(cars?.id, cars?.model))
+        val resource = useCase.invoke(cars?.id, cars?.model)
+        when (resource.status) {
+            Resource.Status.SUCCESS -> {
+                progressState.setPostValue(false)
+                resource.data?.collect {
+                    (years as MutableSharedFlow).emit(it)
+                }
+            }
+            Resource.Status.ERROR -> {
+                progressState.setPostValue(false)
+                errorState.setPostValue(resource.message.toString())
+            }
+            else -> {}
+        }
     }
 }
