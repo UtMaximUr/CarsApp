f4c0fc25f394837e7ae8fd096dfd9596b7bdba88
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-03-10 23:14:02 +0700

    delete baseViewModel, delete state

--- feature_model/src/main/java/com/eg/feature_model/ModelViewModel.kt
+++ feature_model/src/main/java/com/eg/feature_model/ModelViewModel.kt
@@ -1,13 +1,11 @@
 package com.eg.feature_model
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.eg.cars.main.base.BaseViewModel
-import com.eg.core.utils.setPostValue
 import com.eg.domain.entity.Cars
 import com.eg.domain.entity.Model
-import com.eg.domain.entity.Resource
 import com.eg.domain.use_case.GetModelUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -17,26 +15,13 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ModelViewModel @Inject constructor(private val useCase: GetModelUseCase) : BaseViewModel() {
+class ModelViewModel @Inject constructor(private val useCase: GetModelUseCase) : ViewModel() {
 
     val models: Flow<PagingData<Model>> by lazy { MutableSharedFlow(replay = 1) }
 
     fun fetchModel(cars: Cars?) = viewModelScope.launch {
-        val resource = useCase.invoke(cars?.id)
-        when (resource.status) {
-            Resource.Status.LOADING -> {
-                progressState.setPostValue(resource.data == null)
-            }
-            Resource.Status.SUCCESS -> {
-                progressState.setPostValue(resource.data == null)
-                resource.data?.cachedIn(viewModelScope)?.collect {
-                    (models as MutableSharedFlow).emit(it)
-                }
-            }
-            Resource.Status.ERROR -> {
-                progressState.setPostValue(resource.data == null)
-                errorState.setPostValue(resource.message.toString())
-            }
+        useCase.invoke(cars?.id).cachedIn(viewModelScope).collect {
+            (models as MutableSharedFlow).emit(it)
         }
     }
 }
--- feature_year/src/main/java/com/eg/feature_year/YearViewModel.kt
+++ feature_year/src/main/java/com/eg/feature_year/YearViewModel.kt
@@ -1,10 +1,8 @@
 package com.eg.feature_year
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.eg.cars.main.base.BaseViewModel
-import com.eg.core.utils.setPostValue
 import com.eg.domain.entity.Cars
-import com.eg.domain.entity.Resource
 import com.eg.domain.entity.Year
 import com.eg.domain.use_case.GetYearsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,24 +13,13 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class YearViewModel @Inject constructor(private val useCase: GetYearsUseCase) : BaseViewModel() {
+class YearViewModel @Inject constructor(private val useCase: GetYearsUseCase) : ViewModel() {
 
     val years: Flow<List<Year>> by lazy { MutableSharedFlow(replay = 1) }
 
     fun fetchYear(cars: Cars?) = viewModelScope.launch {
-        val resource = useCase.invoke(cars?.id, cars?.model)
-        when (resource.status) {
-            Resource.Status.SUCCESS -> {
-                progressState.setPostValue(false)
-                resource.data?.collect {
-                    (years as MutableSharedFlow).emit(it)
-                }
-            }
-            Resource.Status.ERROR -> {
-                progressState.setPostValue(false)
-                errorState.setPostValue(resource.message.toString())
-            }
-            else -> {}
+        useCase.invoke(cars?.id, cars?.model).collect {
+            (years as MutableSharedFlow).emit(it)
         }
     }
 }
