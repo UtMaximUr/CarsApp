fbb56155b91e21c9e8eae0f9d24c75b73ca618ab
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-03-10 23:15:22 +0700

    delete state

--- feature_year/src/main/java/com/eg/feature_year/ui/YearScreen.kt
+++ feature_year/src/main/java/com/eg/feature_year/ui/YearScreen.kt
@@ -1,12 +1,13 @@
 package com.eg.feature_year.ui
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -15,7 +16,6 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.eg.core.navigation.NavigationDestination
 import com.eg.core.ui.BackNavigateTopBar
-import com.eg.core.ui.ProgressView
 import com.eg.core.ui.TextField
 import com.eg.core.ui.TopBar
 import com.eg.core.utils.toJson
@@ -61,12 +61,6 @@ fun YearScreen(
                     }
                 }
             }
-        }
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            ProgressView(viewModel)
         }
     }
 }
