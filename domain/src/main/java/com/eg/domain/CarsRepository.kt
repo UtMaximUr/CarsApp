a0e1986beae6b46ce2c46169c11f8ff734b2450b
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-03-10 23:15:08 +0700

    refactoring

--- domain/src/main/java/com/eg/domain/CarsRepository.kt
+++ domain/src/main/java/com/eg/domain/CarsRepository.kt
@@ -3,15 +3,14 @@ package com.eg.domain
 import androidx.paging.PagingData
 import com.eg.domain.entity.Brand
 import com.eg.domain.entity.Model
-import com.eg.domain.entity.Resource
 import com.eg.domain.entity.Year
 import kotlinx.coroutines.flow.Flow
 
 interface CarsRepository {
 
-    suspend fun fetchBrands(): Resource<Flow<PagingData<Brand>>>
+    suspend fun fetchBrands(): Flow<PagingData<Brand>>
 
-    suspend fun fetchModels(id: String?): Resource<Flow<PagingData<Model>>>
+    suspend fun fetchModels(id: String?): Flow<PagingData<Model>>
 
-    suspend fun fetchYears(id: String?, name: String?): Resource<Flow<List<Year>>>
+    suspend fun fetchYears(id: String?, name: String?): Flow<List<Year>>
 }
