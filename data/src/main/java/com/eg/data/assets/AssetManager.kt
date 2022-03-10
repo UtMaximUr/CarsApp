c091c171830dec8d911fcd905e927b3d4dfd88e8
Максим Утюжников <maksim.utyuzhnikov@digital-ecosystems.ru>
2022-03-10 23:13:18 +0700

    rename json

--- data/src/main/java/com/eg/data/assets/AssetManager.kt
+++ data/src/main/java/com/eg/data/assets/AssetManager.kt
@@ -1,18 +1,21 @@
 package com.eg.data.assets
 
 import android.content.res.AssetManager
+import com.eg.data.network.entity.CarsResponse
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import java.io.IOException
 
 class AssetManager(private val assetManager: AssetManager) {
 
-    fun getBrandsList(): String {
+    fun getCarsList(): String {
         return getJsonFromAssets()
     }
 
     private fun getJsonFromAssets(): String {
         var jsonString = ""
         try {
-            val inputStream = assetManager.open("brands_list.json")
+            val inputStream = assetManager.open("cars_list.json")
             val size = inputStream.available()
             val buffer = ByteArray(size)
             inputStream.read(buffer)
